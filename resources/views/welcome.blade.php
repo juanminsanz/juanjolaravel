b12f0b270ab584a6e08099aa90ec03a0a2620ab2
juanjo <hermes211@gmail.com>
2018-11-01 02:05:47 +0100

    cambios 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
 
 
-        <title>Laravel</title>
+        <title>Laravel2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
